d69533c9a10376b4db799310f6fea04c1d81b7cd
Queenjiri <ajirijohnsonblessing@gmail.com>
2024-06-29 02:08:47 +0100

    Made the website responsive and added a new font.

--- script.js
+++ script.js
@@ -10,11 +10,14 @@
 // //   for (let i = 0; i < navOpen.length; i++);
 // //
 // // });
+"use strict";
 
 const navOpen = document.querySelector(".open-outline");
 const navClose = document.querySelector(".close-outline");
 
 const navHeader = document.querySelector(".nav-header");
+const iconMain = document.querySelector(".nav-close");
+const showCase = document.querySelector(".icon-btn");
 
 navClose.addEventListener("click", function () {
   navHeader.classList.remove("nav-header");
@@ -23,3 +26,18 @@ navClose.addEventListener("click", function () {
 navOpen.addEventListener("click", function () {
   navHeader.classList.add("nav-header");
 });
+
+// for (let i = 0; i < iconMain.length; i++);
+showCase.addEventListener("click", function () {
+  for (let i = 0; i < iconMain.length; i++);
+  console.log(iconMain);
+  iconMain.classList.toggle("nav-close");
+});
+// addEventListener('click', function(){
+
+// const displayMessage = function () {
+//   iconMain.classList.toggle("nav-close");
+//   iconMain.classList.remove("nav-close");
+// };
+
+// showCase.addEventListener("click", displayMessage());
